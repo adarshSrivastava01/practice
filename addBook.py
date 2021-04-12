5570bf7703e6688e105d9735accfe3c85a0b2136
adarshSrivastava01 <59641575+adarshSrivastava01@users.noreply.github.com>
2021-04-12 19:14:20 +0530

    AddBook Done

--- addBook.py
+++ addBook.py
@@ -2,8 +2,9 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 import urllib
 
+
 def AddBook(id, name, desc, image):
-    
+
     username = urllib.parse.quote_plus('adarsh')
     password = urllib.parse.quote_plus('adarsh123')
 
@@ -19,12 +20,14 @@ def AddBook(id, name, desc, image):
     if not userExists:
         return {"message": "User doesn't exist, try again later.", "status_code": 404}
 
-    userExists["books"].append({
+    newBooks = userExists["books"]
+
+    newBooks.append({
         "name": name,
         "desc": desc,
         "image": image
     })
 
-    r = userCollection.insert_one(userExists)
+    userCollection.update({"_id": ObjectId(id)}, {"$set": {"books": newBooks}})
 
-    print(r)
+    return {"message": "Uploaded Successfully", "status_code": 200}
