f5893333748f196f6a183068feee40421fe0e6dc
Adarsh Srivastava <herecomesthebeastlesnar.com>
2021-04-16 14:36:28 +0530

    Form Created

---
+++ frontend/src/components/Auth.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="container">
+    <h1>The Book Store</h1>
+    <form>
+      <div class="form-control">
+        <label>Name</label>
+        <input
+          type="text"
+          name="name"
+          placeholder="Enter Name"
+          v-model="name"
+        />
+      </div>
+      <div class="form-control">
+        <label>Email</label>
+        <input
+          type="email"
+          name="email"
+          placeholder="Enter Email"
+          v-model="email"
+        />
+      </div>
+      <div class="form-control">
+        <label>Password</label>
+        <input
+          type="password"
+          name="password"
+          placeholder="Enter Password"
+          v-model="password"
+        />
+      </div>
+    </form>
+    <div class="button-box">
+      <Button text="Hello" color="green" />
+      <Button text="Hello" color="green" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Button from "./Button";
+export default {
+  name: "Auth",
+  components: {
+    Button,
+  },
+  data() {
+    return {
+      email: "",
+      password: "",
+      name: "",
+      isLoggedIn: false,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  height: 75vh;
+  width: 30vw;
+  border: 3px solid black;
+  padding: 30px;
+}
+.container h1 {
+  text-align: center;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  margin-bottom: 25px;
+}
+.form-control {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.form-control label {
+  font-size: 24px;
+  margin-bottom: 12px;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+}
+.form-control input {
+  width: 400px;
+  padding: 12px 6px;
+  margin-bottom: 15px;
+}
+.button-box {
+    display: flex;
+    flex-direction: column;
+}
+</style>
---
+++ frontend/src/components/Button.vue
@@ -0,0 +1,33 @@
+<template>
+  <button :style="{ background: color }" @click="onClick()" class="btn">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: String,
+    color: String,
+  },
+  methods: {
+    onClick: () => {
+      console.log("Button Clicked");
+    },
+  },
+};
+</script>
+
+<style scoped>
+button {
+  color: white;
+  border: none;
+  outline: none;
+  margin-top: 8px;
+  margin-bottom: 15px;
+  border-radius: 3px;
+  padding: 12px 24px;
+  cursor: pointer;
+}
+</style>
